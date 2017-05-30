c80b9a59912fa109143b03e812e3f285e58f9dc4
alexandra <alexandra.bjelkholm@gmail.com>
2017-05-30 21:16:24 +0200

    menu js

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
-var menu = document.getElementById('open-menu');
+var tjejerKodar = (function() {
+	var publ = this,
+		priv = {},
+		openMenu = document.getElementById('open-menu'),
+		menu = document.getElementById('main-menu');
 
-document.addEventListener('DOMContentLoaded', function() {
-	menu.classList.remove('js-isOpen');
-	console.log('loaded');
-});
+	priv.init = function () {
+		priv.addEventListeners();
+	};
 
-menu.addEventListener('click', function () {
-	if (this.classList.contains('js-isOpen')) {
-		this.classList.remove('js-isOpen');
-	} else {
-		this.classList.add('js-isOpen');
-	}
-});
+	priv.addEventListeners = function () {
+		document.addEventListener('DOMContentLoaded', function () {
+			openMenu.classList.remove('js-isOpen');
+		});
+
+		openMenu.addEventListener('click', priv.addMenuClass);
+	};
+
+	priv.addMenuClass = function (e) {
+		e.preventDefault();
+
+		if (this.classList.contains('js-isOpen')) {
+			this.classList.remove('js-isOpen');
+			menu.classList.remove('is-open');
+		} else {
+			this.classList.add('js-isOpen');
+			menu.classList.add('is-open');
+		}
+	};
+
+	priv.init();
+
+}(tjejerKodar || {}));
